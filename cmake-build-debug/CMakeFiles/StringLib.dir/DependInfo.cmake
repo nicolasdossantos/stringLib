b8f9925dab64e7b634aabe24ce071a4dd7c2b85e
nicolasdossantos <nicolas182@icloud.com>
2018-04-23 11:13:43 -0400

    First submission

--- cmake-build-debug/CMakeFiles/StringLib.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/StringLib.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/nicolasdossantos/Downloads/StringLib/main.c" "/Users/nicolasdossantos/Downloads/StringLib/cmake-build-debug/CMakeFiles/StringLib.dir/main.c.o"
+  "/Users/nicolasdossantos/Documents/GitHub/StringLib2/main.c" "/Users/nicolasdossantos/Documents/GitHub/StringLib2/cmake-build-debug/CMakeFiles/StringLib.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nicolasdossantos/Downloads/StringLib
+# Install script for directory: /Users/nicolasdossantos/Documents/GitHub/StringLib2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/nicolasdossantos/Downloads/StringLib/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nicolasdossantos/Documents/GitHub/StringLib2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
