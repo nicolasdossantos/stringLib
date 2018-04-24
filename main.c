5b3a43c8a11e1c79b0fd0dfe801222f15fb51090
Thomas Chin <tchin25@gmail.com>
2018-04-24 10:19:34 -0400

    FInishes Replace Function

--- main.c
+++ main.c
@@ -58,12 +58,70 @@ char **str_chop_all(char *s, char c);
 int main() {
 
 
+    printf("%s", replace("hello jello", "ll", "rr"));
 
     return 0;
 }
 
 
 
+char *replace(char *s, char *pat, char *rep){
+    int size = 1;
+    char *cache = malloc(size * sizeof(char));
+    char *cacheWriter = cache;
+    char *sIterator = s;
+    char *sPatChecker;
+    char *patIterator = pat;
+    char *repIterator = rep;
+
+    while (*sIterator != '\0'){
+
+        if(*sIterator == *pat){
+            //check to see if the part is equal
+            int toggle = 1;
+            sPatChecker = sIterator;
+            while(*patIterator != '\0'){
+
+                if (*sPatChecker == *patIterator){
+                    sPatChecker++;
+                    patIterator++;
+                }else{
+                    toggle = 0;
+                    break;
+                }
+            }
+            patIterator = pat; //reset to beginning position
+
+            if (toggle == 0){ //not equal to pat
+                *cacheWriter = *sIterator;
+                cache = realloc(cache, ++size * sizeof(char));
+                cacheWriter++;
+                sIterator++;
+            }else{ //replace pat with rep
+                sIterator = sPatChecker; //pushes iterator to after pat is done
+                while(*repIterator != '\0'){
+                    //copies rep into cache
+                    *cacheWriter = *repIterator;
+                    repIterator++;
+                    cacheWriter++;
+                    cache = realloc(cache, ++size * sizeof(char));
+                }
+                repIterator = rep; //reset to beginning position
+            }
+
+        }else{
+            *cacheWriter = *sIterator;
+            cache = realloc(cache, ++size * sizeof(char));
+            cacheWriter++;
+            sIterator++;
+        }
+    }
+
+    *cacheWriter = '\0';
+
+    return cache;
+
+}
 
 
 int all_letters(char *s) {
@@ -417,9 +475,7 @@ char *repeat(char *s, int x, char sep){
 
 }
 
-char *replace(char *s, char *pat, char *rep){
-
-}
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 char *str_connect(char **strs, int n, char c){
     int i = 0;
